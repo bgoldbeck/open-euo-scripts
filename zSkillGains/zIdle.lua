60a73fd64f8af46103621f5be803bbbbe4fd69b7
bgoldbeck <bgoldbeck21@gmail.com>
2018-06-02 10:20:46 -0700

    renamed skillgains folder

---
+++ zSkillGains/zIdle.lua
@@ -0,0 +1,15 @@
+--Leave alone! (unless you know what to do.)
+dofile("../lib/RLib.lua")   
+
+evalTimer = Timer:New()
+evalTimer:Set(500000)
+                       
+while not Dead(self) do
+
+  if evalTimer:Dif() > 120000 then
+    UseSkill("Evaluating Intelligence")
+    WaitForTarget(2500)
+    Target(self)
+    evalTimer:Clear()      
+  end
+end
